5ca5e49f285d0523b8b0acaedd862d9850e70ab7
huzc <hzc200012@163.com>
2022-07-26 16:00:01 +0800

    项目初始化完成

---
+++ pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="content">
+		<image class="logo animate__bounceIn animate__fast" src="/static/logo.png" v-if="isShow"></image>
+		<view @tap="isShow = !isShow" >
+			<span class="iconfont icon-service" style="font-size: 100upx">
+				
+			</span>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				isShow: true
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
